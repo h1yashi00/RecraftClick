52372120b1afff085930317f89ade01199cad621
h1yashi00 <hayamana3522@gmail.com>
2022-09-20 13:51:59 +0900

    死亡したときのログを改良｡

--- zombiehero/src/main/kotlin/click/recraft/zombiehero/chat/PlayerChatListener.kt
+++ zombiehero/src/main/kotlin/click/recraft/zombiehero/chat/PlayerChatListener.kt
@@ -3,6 +3,7 @@ package click.recraft.zombiehero.chat
 import click.recraft.zombiehero.event.MonsterAttackPlayerEvent
 import click.recraft.zombiehero.event.PlayerDeadPluginHealthEvent
 import click.recraft.zombiehero.monster.api.MonsterManager
+import click.recraft.zombiehero.player.PlayerData.isHeadShot
 import org.bukkit.Bukkit
 import org.bukkit.ChatColor
 import org.bukkit.entity.Player
@@ -10,24 +11,33 @@ import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 
 class PlayerChatListener: Listener {
-
+    private val gun = "\uE003"
+    private val headshotBullet = "\uE001"
+    private val headshotHead = "\uE002"
+    private val zombieAttack = "\uE000"
     private fun Player.coloredName(): String {
         MonsterManager.get(this) ?: return "${ChatColor.WHITE}${name}${ChatColor.GRAY}"
         return "${ChatColor.GREEN}${name}${ChatColor.GRAY}"
     }
 
+    private fun headShotLog(player: Player): String {
+        if (player.isHeadShot()) {
+            return "$gun$headshotBullet$headshotHead"
+        }
+        return gun
+    }
     @EventHandler
     fun playerKillZombie(event: PlayerDeadPluginHealthEvent) {
         val victim = event.victim
         val attacker = event.attacker
         if (attacker !is Player) return
-        Bukkit.broadcastMessage("${victim.coloredName()} was killed ${attacker.coloredName()}")
+        Bukkit.broadcastMessage("${victim.coloredName()} ${headShotLog(victim)} ${attacker.coloredName()}")
     }
 
     @EventHandler
     fun zombieKillPlayer(event: MonsterAttackPlayerEvent) {
         val victim = event.victim
         val attacker = event.attacker
-        Bukkit.broadcastMessage("${victim.coloredName()} was killed ${attacker.coloredName()}")
+        Bukkit.broadcastMessage("${victim.coloredName()} $zombieAttack ${ChatColor.WHITE}${attacker.name}")
     }
 }
