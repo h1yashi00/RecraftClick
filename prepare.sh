fcc7785d88f7f687a6f49f7b0ba00dc3f4c6b563
h1yashi00 <hayamana3522@gmail.com>
2022-10-21 02:10:30 +0900

    socatがいつの間にか落ちていてdockerのデーモンに接続できなくなり､zombiehero鯖が起動できないバグを修正した｡ これからはserviceで管理することにする｡

--- prepare.sh
+++ prepare.sh
@@ -18,8 +18,6 @@ export -f stop_docker_service
 
 # shellcheck disable=SC2046
 docker rm $(docker stop $(docker ps -q))
-pkill -f socat
-socat TCP-LISTEN:2375,reuseaddr,fork UNIX-CONNECT:/var/run/docker.sock &
 set -e
 
 echo "stop_docker_service build_image" | xargs -P 0 -n 1 bash -c
