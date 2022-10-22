99252cc1baec574a6056739f27ec2d14dad176b9
h1yashi00 <hayamana3522@gmail.com>
2022-10-22 14:42:37 +0900

    得られるコインの数をEnumに追加｡

--- share/src/main/kotlin/click/recraft/share/database/Quest.kt
+++ share/src/main/kotlin/click/recraft/share/database/Quest.kt
@@ -2,10 +2,10 @@ package click.recraft.share.database
 
 import org.bukkit.ChatColor
 
-enum class Quest(val id: Int, val finishNum: Int, _displayName: String) {
-    KILL_ZOMBIE(1, 15, "モンスターを倒す"),
-    KILL_HUMAN(2,  5,  "人間を感染させる"),
-    PLAY_TIMES(3, 15,   "プレイした数");
+enum class Quest(val id: Int, val finishNum: Int, _displayName: String, val giveCoin: Int) {
+    KILL_ZOMBIE(1, 15, "モンスターを倒す", 1000),
+    KILL_HUMAN(2,  5,  "人間を感染させる", 1000),
+    PLAY_TIMES(3, 15,   "プレイした数", 1500);
     companion object {
         fun getById(id: Int): Quest? {
             return values().firstOrNull { it.id == id }
